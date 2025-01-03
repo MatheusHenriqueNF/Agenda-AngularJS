d9080a736605e061565465e0519322e73c6b1b29
MatheusHenriqueNF <matheus.henrique26@unifesp.br>
2025-01-03 17:02:32 -0300

    organizando telas

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { SeparadorComponent } from '../../componentes/separador/separador.component';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-contato',
@@ -21,12 +21,20 @@ export class FormularioContatoComponent {
 
   constructor(){
     this.contatoForm = new FormGroup({
-      nome: new FormControl('Nay'),
-      telefone: new FormControl(''),
-      email: new FormControl(''),
+      nome: new FormControl('', Validators.required),
+      telefone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
       aniversario: new FormControl(''),
       redes: new FormControl(''),
       observacoes: new FormControl('')
     })
+  }
+
+  salvarContato(){
+      console.log(this.contatoForm.value);
+  }
+
+  cancelar(){
+    console.log("Submissão cancelada")
   }
 }
